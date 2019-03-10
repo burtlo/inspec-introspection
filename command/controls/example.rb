1b206a336508305b73f6853798f9210c3aa10be7
Lynn Frank <franklin.webber@gmail.com>
2019-03-09 21:24:51 -0600

    Adds matchers to the Cmd definition

--- command/controls/example.rb
+++ command/controls/example.rb
@@ -4,23 +4,21 @@ require 'ostruct'
 
 class Inspec::Resources::Cmd
 
-  # Break initialize
-  def initialize(*args)
-    Resources::Cmd.resource_parameters.each_with_index do |rp, index|
-      instance_exec(args[index], &rp.post_initialize_block)
-    end
-  end
-  
   def self.resource_parameter(name, details, &post_initialize_block)
     resource_parameters.push(OpenStruct.new({name: name, post_initialize_block: post_initialize_block}.merge(details)))
   end
 
+  # This stores all the of the resource parameters defined.
+  #
+  # NOTE: This should likely be added some large meta definition of the Resource
+  #   to reduce the surface area introduced into the class object with these very
+  #   generic names.
   def self.resource_parameters
     @resource_parameters ||= []
   end
 
   resource_parameter 'cmd', type: [:to_s], required: true, is_identifier: true do |cmd|
-    # TODO: add type validation
+    # TODO: type validation could be done automatically based on the type value if one has been defined.
     if cmd.nil?
       raise 'InSpec `command` was called with `nil` as the argument. This is not supported. Please provide a valid command instead.'
     end
@@ -28,8 +26,9 @@ class Inspec::Resources::Cmd
   end
   
   resource_parameter 'options', type: Hash, required: false, is_identifier: false do |options|
-    # TODO: make the default value more clear
-    options ||= {}
+    # TODO: default values could be used instead of explictly stating required false
+    default_options = {}
+    options ||= default_options
 
     if options[:redact_regex]
       unless options[:redact_regex].is_a?(Regexp)
@@ -42,22 +41,43 @@ class Inspec::Resources::Cmd
     end
   end
 
+  # REPLACE - #initialize
+  #
+  # As a resource can define N number of resource parameters I needed
+  # to replace the initialize argument signature with one that could
+  # take N arguments.
+  #
+  # TODO: The specific class object needs to be used because of the way that InSpec generates
+  #    resources. The class object could probably be discovered at runtime in a more generic
+  #    way
+  #
+  def initialize(*args)
+    # NOTE: a `pre_initialize` invokation by default with every resource defining an no-op method 
+    Resources::Cmd.resource_parameters.each_with_index do |rp, index|
+      instance_exec(args[index], &rp.post_initialize_block)
+    end
+    # NOTE: a `post_initialize` invokation by default with every resource defining an no-op method 
+  end
+  
 
   # Break the Properties that we define
   def result ; end
   def stdout ; end
 
-  # Defines a property
+  # Class level method that enables the definition of a property
   def self.property(name, details, &block)
     properties.push(OpenStruct.new({name: name }.merge(details)))
     define_method name, &block
   end
-  # Stores the properties
+
+  # Stores all the properties
   # NOTE: consider creating a meta definiton of the object which stores all this information
   def self.properties
     @properties ||= []
   end
 
+  # result is defined here as a public property. If it was not meant to be public an instance_methdod
+  # could be defined or this property definition could be marked as private
   property 'result', {} do
     @result ||= inspec.backend.run_command(@command)
   end
@@ -72,13 +92,51 @@ EXAMPLE
   } do
     result.stdout
   end
-  
+
+  def self.matcher(name, details, &block)
+    matchers.push(OpenStruct.new({name: name }.merge(details)))
+    define_method name, &block
+  end
+
+  def self.matchers
+    @matchers ||= []
+  end
+
+  matcher 'exist?', { desc: 'Test if the command exists', example: <<-EXAMPLE
+describe command('echo') do
+  it { should exist }
+end
+EXAMPLE
+      } do
+    # silent for mock resources
+    return false if inspec.os.name.nil? || inspec.os.name == 'mock'
+
+    if inspec.os.linux?
+      res = if inspec.platform.name == 'alpine'
+              inspec.backend.run_command("which \"#{@command}\"")
+            else
+              inspec.backend.run_command("bash -c 'type \"#{@command}\"'")
+            end
+    elsif inspec.os.windows?
+      res = inspec.backend.run_command("Get-Command \"#{@command}\"")
+    elsif inspec.os.unix?
+      res = inspec.backend.run_command("type \"#{@command}\"")
+    else
+      warn "`command(#{@command}).exist?` is not supported on your OS: #{inspec.os[:name]}"
+      return false
+    end
+    res.exit_status.to_i == 0
+  end
 end
 
-describe command('echo hello') do
+describe command('echo hello') do  
   its(:stdout) { should eq "hello\n" }
   its(:stderr) { should be_empty }
   its(:exit_status) { should eq 0 }
+end
+
+describe command('ls') do
+  it { should exist }
 end
 
 describe 'Cmd' do
@@ -139,7 +197,6 @@ describe 'Cmd' do
   end
 
   describe 'properties' do
-
     let(:properties) { resource.properties }
 
     # property - name, type, description, example,
@@ -189,13 +246,30 @@ EXAMPLE
     end
 
   end
-  
+
+  describe 'matchers' do
+    # matcher - name, args (array of hash, name, type description), description, example, permissions_required, see_also
+    let(:matchers) { resource.matchers }
+
+    describe 'exist?' do
+      let(:name) { 'exist?' }
+      let(:subject) { matchers.find { |p| p.name == name } }
+      let(:expected_desc) do
+        'Test if the command exists'
+      end
+      let(:expected_example) do
+        <<-EXAMPLE
+describe command('echo') do
+  it { should exist }
 end
+EXAMPLE
 
-# matcher - name, args (array of hash, name, type description), descrioption, example, permissions_required, see_also
-# filter_criterion - name, type, description, example, permissions_required
+      end
 
-# limitation
-# related_resource
+      its('name') { should eq name }
+      its('desc') { should eq expected_desc }
+      its('example') { should eq expected_example }
+    end
+  end
 
-
+end
